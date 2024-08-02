5de4b0906e01f875e2dea52c337868c4f8088ce5
TM-Code-009 <osakweterrynduka@gmail.com>
2024-08-02 23:36:39 +0100

    portfixed

--- app/component/Hero/page.tsx
+++ app/component/Hero/page.tsx
@@ -7,7 +7,7 @@ const page = () => {
      <main className='w-full min-h-[500px] flex flex-col-reverse lg:flex-row items-center justify-between py-8 lg:py:0'>
         <div className='flex flex-col gap-2'>
         <p className='text-[14px] text-green-400'>hello!</p>
-        <p className='lg:text-[70px] text-center lg:text-left text-[60px] font-bold text-white'>i'm <span className='text-green-400'>Terry<br/>Osakwe</span></p>
+        <p className='lg:text-[70px] text-center lg:text-left text-[60px] font-bold text-white'>i am <span className='text-green-400'>Terry<br/>Osakwe</span></p>
         <p className='text-white text-[14px] lg:text-[20px] text-center'>A Freelancer Software-Engineer</p>
     </div>
     <div>
